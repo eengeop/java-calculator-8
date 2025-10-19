8ccf8fd31157d7d8859967f3e6cc00891d6acc42
eengeop <leeeus99@gmail.com>
2025-10-19 22:46:12 +0900

    feat: 사용자의 입력을 받아서 Service 계층으로 넘겨주는 View 계층

---
+++ src/main/java/calculator/view/InputView.java
@@ -0,0 +1,13 @@
+package calculator.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public static final String INIT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+
+    public String getUserInput() {
+        System.out.println(INIT_MESSAGE);
+        return Console.readLine();
+    }
+}
