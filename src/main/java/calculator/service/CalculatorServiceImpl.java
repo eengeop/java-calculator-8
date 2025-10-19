fb27a3356951a2b744caf6d571fe4af9ab6b42ef
eengeop <leeeus99@gmail.com>
2025-10-19 22:40:38 +0900

    feat: 기본 구분자와 커스텀 구분자를 리스트에 담고 숫자 사이를 쉼표로 구분하는 Model 계층에 의해 호출 되는 Service 계층 함수

--- src/main/java/calculator/service/CalculatorServiceImpl.java
+++ src/main/java/calculator/service/CalculatorServiceImpl.java
@@ -11,6 +11,7 @@ public class CalculatorServiceImpl implements CalculatorService {
 
     @Override
     public String findNumber(String userInput) {
+
         List<Character> allDelimiters = new ArrayList<>();
         String trimedString = deleteCustomDefine(userInput, allDelimiters);
 
@@ -21,43 +22,5 @@ public class CalculatorServiceImpl implements CalculatorService {
         String replacedString = replaceCustomToDefault(trimedString, allDelimiters);
 
         return replacedString;
-    }
-
-    private String deleteCustomDefine(String userInput, List<Character> allDelimiters) {
-        StringBuilder sb = new StringBuilder(userInput);
-        int searchIndex = 0;
-
-        while (sb.indexOf(START_CUSTOM_DELIMITER) != -1) {
-            int startIndex = sb.indexOf(START_CUSTOM_DELIMITER, searchIndex);
-            int endIndex = sb.indexOf(END_CUSTOM_DELIMITER, startIndex);
-
-            if (endIndex - startIndex != 3) {
-                System.out.println(endIndex - startIndex);
-                throw new IllegalArgumentException("구분자는 단일문자만 가능합니다.");
-            }
-
-            int customDelimiterIndex = sb.indexOf(START_CUSTOM_DELIMITER) + 2;
-            char customDelimiter = sb.charAt(customDelimiterIndex);
-
-            if(Character.getNumericValue(customDelimiter) != -1){
-                throw new IllegalArgumentException("숫자는 커스텀 구분자가 될 수 없습니다.");
-            }
-
-            allDelimiters.add(customDelimiter);
-            sb.delete(startIndex, endIndex + 2);
-            searchIndex = startIndex;
-        }
-        return sb.toString();
-    }
-
-    private String replaceCustomToDefault(String trimedString, List<Character> allDelimiters) {
-        StringBuilder sb = new StringBuilder(trimedString);
-
-        for (int i = 0; i < trimedString.length(); i++) {
-            if (allDelimiters.contains(sb.charAt(i))) {
-                sb.replace(i, i + 1, ",");
-            }
-        }
-        return sb.toString();
     }
 }
